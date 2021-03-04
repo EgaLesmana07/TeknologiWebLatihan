eef80787ceb7af0e1ea75393856c0a747da5f9e0
EgaLesmana07 <lesmanaega7@gmail.com>
2021-03-03 20:17:28 -0800

    Aku Adalah Dia

---
+++ rayy/src/rayy/Rayy.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rayy;
+
+/**
+ *
+ * @author SOFTWARE
+ */
+public class Rayy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // membuat array
+        String[] teman = {"Linda", "Santi", "Susan", "Mila", "Ayu"};
+        // mengambil data array
+        for (int i= 0; i < teman.length; i++) {
+            System.out.println("Indexs ke "+i+ " : " + teman[i]);
+        }
+            
+        }
+    }
+   
